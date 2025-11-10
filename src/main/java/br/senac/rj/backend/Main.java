cff18b03902a417a93259f9d667e26cb3f7e3117
Reinaldo José de Freits <reinaldo.freitas@rj.senac.br>
2025-11-09 21:25:17 -0300

    Ajuste de conflito

--- src/main/java/br/senac/rj/backend/Main.java
+++ src/main/java/br/senac/rj/backend/Main.java
@@ -40,7 +40,7 @@ public class Main {
         // Inicia o servidor
         server.start();
         System.out.println("Servidor iniciado em http://localhost:" + port + "/api");
-        System.out.println("Alteração feita por mimm");
+        System.out.println("Alteração feita por mim");
         
         server.join();
     }
