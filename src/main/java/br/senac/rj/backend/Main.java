af2f12f2d2ca4fdff3ea788b5a68cad1840d7f96
reinaldojfreitas <100420836+reinaldojfreitas@users.noreply.github.com>
2025-11-09 21:22:41 -0300

    Update Main.java

--- src/main/java/br/senac/rj/backend/Main.java
+++ src/main/java/br/senac/rj/backend/Main.java
@@ -40,7 +40,7 @@ public class Main {
         // Inicia o servidor
         server.start();
         System.out.println("Servidor iniciado em http://localhost:" + port + "/api");
-        System.out.println("Alteração feita por mimm");
+        System.out.println("Alteração feita pelo colega de equipe");
         
         server.join();
     }
